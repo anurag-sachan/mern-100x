6053064d93627046d3dd388ec27e27ba1f39ae37
Anurag Sachan <connect2anurags@gmail.com>
2024-08-21 19:07:49 +0530

    basic backend: server + api

---
+++ backend/server.jsx
@@ -0,0 +1,15 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app=express()
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    fs.readFile(path.join(__dirname,'courses.json'),'utf8',(err,data)=>{
+        if(err) return res.status(500);
+        return res.status(200).json(JSON.parse(data));
+    })
+})
+
+app.listen(3001);
